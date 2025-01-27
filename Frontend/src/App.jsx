5a9074a51c3ed70bdc49bcd604d51049c183c351
Gupta <c2gupt@gmail.com>
2025-01-27 23:04:36 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import {Button} from './components/ui/Button'
+function App() {
+  
+
+  return (
+    <>
+
+<Button/>
+    </>
+  )
+}
+
+export default App
