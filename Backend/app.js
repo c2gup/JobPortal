2edbb78ce88d5fef7ba3ef0d59cb21b221d20588
Gupta <c2gupt@gmail.com>
2025-02-17 19:58:32 +0530

    day3 done backend done

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,9 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import dbconnect from "./config/db.js";
+import companyroute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
 
 
 
@@ -25,6 +28,10 @@ app.get("/", (req, res) => {
 
 
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyroute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
+
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
--- Backend/routes/application.route.js
+++ Backend/routes/application.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
-import { applyJob, getApplicants, getAppliedJobs, updateStatus } from "../controllers/application.controller.js";
+import { applyJob, getApplicants, getAppliedJobs, updateStatus } from "../controller/application.controller.js";
  
 const router = express.Router();
 
